f9a0dab8cca2771bc658d8bc02a002eb594c4ccf
Kris Gano <46331884+pyxld-kris@users.noreply.github.com>
2019-08-13 15:00:20 -0500

    Implemented basic code/page structure for rendering learning activities
    Needs css styling!

---
+++ day-one-devs.js
@@ -0,0 +1,13 @@
+function buttonClick(){
+  var userInput = prompt("is this the day one dev website");
+
+  if(userInput == "yes"){
+    alert("nice");
+  }
+  else if (userInput == "no") {
+    alert("dang");
+  }
+  else{
+    alert("the only two options are yes or no");
+  }
+}
