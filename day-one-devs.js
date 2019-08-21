f3adc9e10a4db214a1084498e4c60486b2f252be
Kris Gano <46331884+pyxld-kris@users.noreply.github.com>
2019-08-21 17:23:48 -0500

    Modified landing page layout. Tidied up a bit.

--- day-one-devs.js
+++ day-one-devs.js
@@ -1,13 +1,11 @@
-function buttonClick(){
+function buttonClick() {
   var userInput = prompt("is this the day one dev website");
 
-  if(userInput == "yes"){
+  if (userInput == "yes") {
     alert("nice");
-  }
-  else if (userInput == "no") {
+  } else if (userInput == "no") {
     alert("dang");
-  }
-  else{
+  } else {
     alert("the only two options are yes or no");
   }
 }
